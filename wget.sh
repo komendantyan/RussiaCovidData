51f4f00632df6bdfc25af110c5447a8f96401ea2
Andrey Komendantyan <komendantyan@gmail.com>
2020-04-24 12:25:38 +0300

    add archive of pages with tests count

--- wget.sh
+++ wget.sh
@@ -5,3 +5,10 @@ date=`jq -rn "$last | .date"`
 link=`jq -rn "$last | .link"`
 
 wget $link -O archive/$date.html
+
+
+last=`jq '.[0]|{date,link}' tests.json`
+date=`jq -rn "$last | .date"`
+link=`jq -rn "$last | .link"`
+
+wget $link -O archive_info/$date.html
