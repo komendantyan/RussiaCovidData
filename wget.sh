3e00200edf078efbc3481753decb73c0d7961f43
Andrey Komendantyan <komendantyan@gmail.com>
2020-11-08 14:51:53 +0300

    update 7-8 nov

--- wget.sh
+++ wget.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-last=`jq '.[0]|{date,link}' corona.json`
-date=`jq -rn "$last | .date"`
-link=`jq -rn "$last | .link"`
+for n in {0..1}; do
+    last=`jq ".[$n]|{date,link}" corona.json`
+    date=`jq -rn "$last | .date"`
+    link=`jq -rn "$last | .link"`
 
-wget $link -O archive/$date.html
-
-
-last=`jq '.[0]|{date,link}' tests.json`
-date=`jq -rn "$last | .date"`
-link=`jq -rn "$last | .link"`
-
-wget $link -O archive_info/$date.html
+    wget $link -O archive/$date.html
+done
